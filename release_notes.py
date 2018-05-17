c066815b9c5859af0dc30490c89b2df5f73e38a8
Lior Blob <liorb@demisto.com>
2018-05-17 16:43:48 +0300

    why error

--- release_notes.py
+++ release_notes.py
@@ -520,6 +520,7 @@ def createContentDescriptor(version, assetId, res):
         json.dump(contentDescriptor, outfile)
 
     with open('release-notes.txt', 'w') as outfile:
+        print release_notes
         outfile.write(release_notes)
 
 
